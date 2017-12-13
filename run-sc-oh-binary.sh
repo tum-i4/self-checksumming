f750905be00cb1858d2ce8278bd66e64e3a36ff0
Mohsen Ahmadvand <mohsen.ahmadvand@tum.de>
2017-12-13 12:49:51 +0000

    config line added

--- run-sc-oh-binary.sh
+++ run-sc-oh-binary.sh
@@ -19,8 +19,6 @@ if [ $? -eq 0 ]; then
 
 
 clang-3.9 $1 -c -emit-llvm -o guarded.bc
-clang-3.9 rtlib.c -c -emit-llvm -o rtlib.bc
-llvm-link-3.9 guarded.bc rtlib.bc -o guarded.bc
 
 echo 'Remove old files'
 rm guide.txt
@@ -38,7 +36,8 @@ input=$2
 
 echo 'Transform SC & OH'
 #opt-3.9 -load $INPUT_DEP_PATH/libInputDependency.so - -load $UTILS_LIB -load $SC_PATH/libSCPass.so -load $OH_LIB/liboblivious-hashing.so $bitcode -sc -dump-checkers-network="$ASSERT_SKIP_FILE" -skip 'hash' -oh-insert -num-hash 1 -o out.bc
-opt-3.9 -load $INPUT_DEP_PATH/libInputDependency.so -load $UTILS_LIB -load $SC_PATH/libSCPass.so -load $OH_LIB/liboblivious-hashing.so -load $INPUT_DEP_PATH/libTransforms.so $bitcode 	-clone-functions -extract-functions -sc -connectivity=5  -dump-checkers-network="network_file" -dump-stat="oh_sc_stats" -oh-insert -num-hash 1 -o out.bc   
+opt-3.9 -load $INPUT_DEP_PATH/libInputDependency.so -load $UTILS_LIB -load $SC_PATH/libSCPass.so -load $OH_LIB/liboblivious-hashing.so -load $INPUT_DEP_PATH/libTransforms.so $bitcode -lib-config=/home/sip/input-dependency-analyzer/library_configs/tetris_library_config.json -clone-functions -extract-functions -sc -connectivity=5  -dump-checkers-network="network_file" -dump-sc-stat="sc.stats" -filter-file="" -oh-insert -num-hash 1 -dump-oh-stat="oh.stats" -extraction-stats -extraction-stats-file="extract.stats" -clone-stats -clone-stats-file="clone.stats" -dependency-stats -dependency-stats-file="dependency.stats" -o out.bc 
+
 
 if [ $? -eq 0 ]; then
 	    echo 'OK Transform'
@@ -50,6 +49,11 @@ if [ $? -eq 0 ]; then
 
 # compiling external libraries to bitcodes
 clang-3.9 $OH_PATH/assertions/response.c -c -fno-use-cxa-atexit -emit-llvm -o $OH_PATH/assertions/response.bc
+
+
+#link guardMe function
+clang-3.9 rtlib.c -c -emit-llvm -o rtlib.bc
+llvm-link-3.9 out.bc rtlib.bc -o out.bc
 
 
 echo 'Post patching binary after hash calls'
