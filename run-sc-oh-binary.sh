6711e6ac609a84515ad61ac2f31d2988c6a30d3a
anahitH <anahithayrapetyan91@gmail.com>
2018-09-10 13:27:52 +0200

    Updated scripts to link rtlibs dynamically

--- run-sc-oh-binary.sh
+++ run-sc-oh-binary.sh
@@ -36,7 +36,7 @@ input=$2
 
 echo 'Transform SC & OH'
 #opt-3.9 -load $INPUT_DEP_PATH/libInputDependency.so - -load $UTILS_LIB -load $SC_PATH/libSCPass.so -load $OH_LIB/liboblivious-hashing.so $bitcode -sc -dump-checkers-network="$ASSERT_SKIP_FILE" -skip 'hash' -oh-insert -num-hash 1 -o out.bc
-opt-3.9 -load $INPUT_DEP_PATH/libInputDependency.so -load $UTILS_LIB -load $SC_PATH/libSCPass.so -load /usr/local/lib/libLLVMdg.so -load $OH_LIB/liboblivious-hashing.so -load $INPUT_DEP_PATH/libTransforms.so $bitcode -strip-debug -unreachableblockelim -globaldce -lib-config=/home/sip/input-dependency-analyzer/library_configs/tetris_library_config.json -extract-functions -sc -connectivity=2 -maximum-input-independent-percentage=100 -dump-checkers-network="network_file" -dump-sc-stat="sc.stats" -filter-file="" -oh-insert -short-range-oh -num-hash 1 -dump-oh-stat="oh.stats" -extraction-stats -extraction-stats-file="extract.stats" -dependency-stats -dependency-stats-file="dependency.stats" -o out.bc
+opt-6.0 -load $INPUT_DEP_PATH/libInputDependency.so -load $UTILS_LIB -load $SC_PATH/libSCPass.so -load /usr/local/lib/libLLVMdg.so -load $OH_LIB/liboblivious-hashing.so -load $INPUT_DEP_PATH/libTransforms.so $bitcode -strip-debug -unreachableblockelim -globaldce -lib-config=/home/sip/input-dependency-analyzer/library_configs/tetris_library_config.json -extract-functions -sc -connectivity=2 -maximum-input-independent-percentage=100 -dump-checkers-network="network_file" -dump-sc-stat="sc.stats" -filter-file="" -oh-insert -short-range-oh -num-hash 1 -dump-oh-stat="oh.stats" -extraction-stats -extraction-stats-file="extract.stats" -dependency-stats -dependency-stats-file="dependency.stats" -o out.bc
 
 if [ $? -eq 0 ]; then
 	    echo 'OK Transform'
@@ -45,31 +45,26 @@ if [ $? -eq 0 ]; then
 	       exit	
 	fi
 
-
-# compiling external libraries to bitcodes
-clang-3.9 $OH_PATH/assertions/response.c -c -fno-use-cxa-atexit -emit-llvm -o $OH_PATH/assertions/response.bc
-
-#link guardMe function
-clang-3.9 rtlib.c -c -emit-llvm -o rtlib.bc
-
-llvm-link-3.9 out.bc rtlib.bc -o out.bc
-
 echo 'Post patching binary after hash calls'
-llc-3.9 out.bc
+llc-6.0 out.bc
+g++ -std=c++0x -c -rdynamic out.s -o out.o
 
-gcc -c -rdynamic out.s -o out.o -lncurses
+LIB_FILE=()
+g++ -fPIC -std=c++11 -g -rdynamic -c ${OH_PATH}/assertions/response.cpp -o oh_rtlib.o
+LIB_FILES+=( "${OH_PATH}/assertions/oh_rtlib.o" )
 
-# Linking with external libraries
-gcc -g -rdynamic -c $OH_PATH/assertions/response.c -o response.o
+gcc -fPIC -g -rdynamic -c "${PWD}/rtlib.c" -o "${PWD}/sc_rtlib.o"
+LIB_FILES+=( "${PWD}/sc_rtlib.o" )
 
-#gcc -g -rdynamic -c rtlib.c -o rtlib.o
-gcc -g -rdynamic out.o response.o -o out -lncurses 
+g++ -std=c++11 -g -rdynamic -Wall -fPIC -shared -Wl,-soname,libsrtlib.so -o "librtlib.so" -lncurses -lm -lssl -lcrypto -pthread ${LIB_FILES[@]}
 
-#clang++-3.9 -lncurses -rdynamic -std=c++0x out.bc -o out
+g++ -std=c++11 -g -rdynamic out.o -o out -L. -lrtlib -lncurses -lm -lssl -lcrypto -pthread
+
 python patcher/dump_pipe.py out guide.txt patch_guide
 echo 'Done patching'
 
 #Patch using GDB
-python $OH_PATH/patcher/patchAsserts.py -b out -n out_patched -s oh.stats
-
+export LD_PRELOAD="/home/sip/self-checksumming/hook/build/libminm.so ${PWD}/librtlib.so" 
+args=' < /home/anahitik/SIP/sip-eval/my_cmdline-args/rawcaudio_small.pcm > output_small.adpcm'
+python $OH_PATH/patcher/patchAsserts.py -b out -n out_patched -s oh.stats -g "$args" -p "${PWD}/gdb_script_for_do_assert.txt"
 chmod +x out_patched
